b3ef654bdf94d3896dd20b965bc0595b671a5b6a
gferreira <gferreiraa@gmail.com>
2018-12-08 00:32:27 -0200

    Animations

--- app-instagram/src/app/app.module.ts
+++ app-instagram/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -16,7 +18,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
