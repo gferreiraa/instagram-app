54648835bbe1a0abb5a0a19c56cf10149de812e7
gferreira <gferreiraa@gmail.com>
2018-12-09 22:03:22 -0200

    Animation implements

--- app-instagram/src/app/acesso/banner/banner.component.ts
+++ app-instagram/src/app/acesso/banner/banner.component.ts
@@ -24,7 +24,7 @@ export class BannerComponent implements OnInit {
   public estado = 'escondido';
 
   public imagens: Imagem[] = [
-    { estado: 'escondido', url: '/assets/banner-acesso/img_1.png'},
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png'},
     { estado: 'escondido', url: '/assets/banner-acesso/img_2.png'},
     { estado: 'escondido', url: '/assets/banner-acesso/img_3.png'},
     { estado: 'escondido', url: '/assets/banner-acesso/img_4.png'}
@@ -33,7 +33,26 @@ export class BannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.imagens);
+    setTimeout(() => this.logicaRotacao(), 4000);
   }
 
+  public logicaRotacao(): void {
+    // Auxilia na exibição da imagem seguinte
+    let idx: number;
+
+    // Ocultar imagens
+    for (let i = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+        idx = i === 4 ? 0 : i + 1;
+        break;
+      }
+    }
+
+    // Exibir imagens
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), 4000);
+
+  }
 }
