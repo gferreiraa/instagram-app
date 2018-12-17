2fcbedfaa13bd83b15be202eca8563719d0ece55
gferreira <gferreiraa@gmail.com>
2018-12-16 23:11:09 -0200

    Config Firebase

--- app-instagram/src/app/app.component.ts
+++ app-instagram/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+// * asterisco para utilização de alias.
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-instagram';
+
+  ngOnInit(): void {
+    // Configuração com o Firebase
+    var config = {
+      apiKey: 'AIzaSyBIz55M9y-8ya45CyX2F_-pnEbVhdTW0Wo',
+      authDomain: 'instagram-clone-e83ef.firebaseapp.com',
+      databaseURL: 'https://instagram-clone-e83ef.firebaseio.com',
+      projectId: 'instagram-clone-e83ef',
+      storageBucket: 'instagram-clone-e83ef.appspot.com',
+      messagingSenderId: '498663363761'
+    };
+    firebase.initializeApp(config);
+  }
 }
