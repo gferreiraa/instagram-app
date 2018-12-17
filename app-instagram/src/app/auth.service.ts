cae09fe9a3a8a4d98cb74c3794ec7b97233d04d4
gferreira <gferreiraa@gmail.com>
2018-12-16 23:35:06 -0200

    Created entry record in Firebase

--- app-instagram/src/app/auth.service.ts
+++ app-instagram/src/app/auth.service.ts
@@ -1,8 +1,18 @@
 import { Usuario } from './acesso/usuario,model';
+import * as firebase from 'firebase';
 
 export class Autenticacao {
   // Metodo para receber user model
   public cadastrarUsuario(usuario: Usuario): void {
     console.log('Chegamos até aqui', usuario);
+
+    firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.senha)
+      .then( (response: any) => {
+        console.log('Usuário Cadastrado', response);
+      })
+      .catch( (error: Error) => {
+        console.log(error);
+      });
+
   }
 }
