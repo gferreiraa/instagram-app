892b7d33de9a6a0c6252cc2c4f69c73eaa31a191
gferreira <gferreiraa@gmail.com>
2018-12-19 00:12:54 -0200

    Created Routers

---
+++ app-instagram/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { HomeComponent } from './home/home.component';
+import { AcessoComponent } from './acesso/acesso.component';
+import { Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  { path: '', component: AcessoComponent },
+  { path: 'home', component: HomeComponent },
+];
