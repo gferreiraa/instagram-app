0373fe0699bb49d8e81e2d270d9b5e60afbc1782
gferreira <gferreiraa@gmail.com>
2018-12-24 23:48:03 -0200

    Logout

--- app-instagram/src/app/home/home.component.ts
+++ app-instagram/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Autenticacao } from './../auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private autenticacao: Autenticacao
+  ) { }
 
   ngOnInit() {
+  }
+
+  public sair(): void {
+    this.autenticacao.sair()
   }
 
 }
