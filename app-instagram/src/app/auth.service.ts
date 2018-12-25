d88860b2e917fc8a2afc918d69f53e38603295cd
gferreira <gferreiraa@gmail.com>
2018-12-25 00:09:07 -0200

    Update: Logout feature

--- app-instagram/src/app/auth.service.ts
+++ app-instagram/src/app/auth.service.ts
@@ -47,4 +47,13 @@ export class Autenticacao {
     }
     return this.token_id !== undefined;
   }
+
+  public sair(): void {
+    firebase.auth().signOut()
+      .then( () => {
+        localStorage.removeItem('idToken');
+        this.token_id = undefined;
+        this.router.navigate(['/']);
+      });
+  }
 }
